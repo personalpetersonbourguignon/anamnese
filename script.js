44be9adfc3999ae0721c3364f9ded31b1187a43b
personalpetersonbourguignon <personalpetersonbourguignon@gmail.com>
2025-10-22 17:53:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('anamneseForm').addEventListener('submit', async functio
   const dados = Object.fromEntries(formData.entries());
 
   // Envio para o Google Apps Script
-  const scriptURL = https://script.google.com/macros/s/AKfycbzZAsl-TNcIlQnjrd6e9oX0Z9-lymW7xVnbdVyRrOL9nTs7qXffY0GztufRZewGjA0Eng/exec;
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw9Xjm-0nRZvr8PmBLVlwtBVD6jyT-vCwNUb3OjLsGTqwh7uNmFk9x8pgEwRSkM9RHZCg/exec';
   
   const response = await fetch(scriptURL, {
     method: 'POST',
